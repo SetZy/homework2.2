9f094074ab0bd2630506fd765ff759fb6d64f03d
setZ <ramiras.ukr@gmail.com>
2018-11-08 13:25:55 +0200

    fixed enter to console

--- 4/common1.js
+++ 4/common1.js
@@ -1,19 +1,21 @@
 var arr = ['g', 'd', 'r'];
 
 
-function wer(arr){
+function wer(ar){
 	var obj = {};
 	var a = '';
-	for(var i=0; i<arr.length; i++){
-		a = arr[i];
+	for(var i=0; i<ar.length; i++){
+		a = ar[i];
 		obj[a] = i;
 		a = '';
 		
 	}
 	return obj;
+	console.log(obj);
 }
 
 console.log(arr);
-for(var k in wer(arr)){
+console.log(wer(arr));
+/*for(var k in wer(arr)){
 	console.log(k + ":" + wer(arr)[k]);
-}
+	}*/
